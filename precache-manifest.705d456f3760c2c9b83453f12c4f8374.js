c3330abd0361c7e02bbf7b0e063eeb050744b036
MatheusBlanco <msallesblanco@gmail.com>
2018-12-17 15:14:46 -0200

    Updates

---
+++ precache-manifest.705d456f3760c2c9b83453f12c4f8374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e64140a08d85be2eb732",
+    "url": "/git-resume/static/js/runtime~main.e64140a0.js"
+  },
+  {
+    "revision": "f4fcdf77de4a8b4f6580",
+    "url": "/git-resume/static/js/main.f4fcdf77.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/git-resume/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "f4fcdf77de4a8b4f6580",
+    "url": "/git-resume/static/css/main.62c59b7b.chunk.css"
+  },
+  {
+    "revision": "7722c51d8c3705bd5834a42f44105e37",
+    "url": "/git-resume/index.html"
+  }
+];
